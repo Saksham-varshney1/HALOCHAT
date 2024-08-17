1c9097f572929b9e94503c644cd91d86987683fa
Saksham-varshney1 <varshneysaksham5@gmail.com>
2024-08-17 23:30:18 +0530

    Real time Varshney-chat-app

--- app/(root)/layout.js
+++ app/(root)/layout.js
@@ -7,7 +7,7 @@ import BottomBar from "@components/BottomBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Halo Chat App",
+  title: "Varshney Chat App",
   description: "A Next.js 14 Chat App ",
 };
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -61,7 +61,7 @@ const Form = ({ type }) => {
   return (
     <div className="auth">
       <div className="content">
-        <img src="/assets/logo.png" alt="logo" className="logo" />
+        <img src="/assets/logo2.png" alt="logo" className="logo" />
 
         <form className="form" onSubmit={handleSubmit(onSubmit)}>
           {type === "register" && (
